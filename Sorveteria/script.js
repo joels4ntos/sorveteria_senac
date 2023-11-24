3fc9a18b6fca4c3e0284a74a39b416e8ba5984c2
joels4ntos <joel.emete34@gmail.com>
2023-11-24 17:23:49 -0300

    insercao de javascrip
    transições com javascript e implementação de css

---
+++ Sorveteria/script.js
@@ -0,0 +1,10 @@
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.sobre', { duration: 2000 });
+
+sr.reveal('.sabores', { duration: 2000 });
+
+sr.reveal('#orcamento', { duration: 2000 });
+
+sr.reveal('.banner', { duration: 2000 });
